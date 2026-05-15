d9c665f3882f28f75bc2c820e8fcedba78e286d2
yogitagavkar <gavkaryogita@gmail.com>
2026-05-15 19:34:57 +0530

    fix:docker file fix

--- fastapi-agent/app/main.py
+++ fastapi-agent/app/main.py
@@ -1,12 +1,28 @@
-from fastapi import FastAPI, UploadFile, Form
+from fastapi import FastAPI, UploadFile, Form,HTTPException,File
 from app.graph.workflow import run_graph
 
 app = FastAPI()
 
 @app.post("/analyze")
-async def analyze(
-    resume: UploadFile,
+async def analyze_resume(
+    resume: UploadFile = File(...),
     job_description: str = Form(...)
 ):
-    result = await run_graph(resume, job_description)
-    return result
+
+    try:
+
+        result = await run_graph(
+            resume,
+            job_description
+        )
+
+        return result
+
+    except Exception as e:
+
+        print("ERROR =>", str(e))
+
+        raise HTTPException(
+            status_code=500,
+            detail=str(e)
+        )
